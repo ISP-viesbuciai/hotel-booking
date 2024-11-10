116a0b143406ff6cad8dc776897e8f33b2e4c9de
Stepon4s <157494962+Stepon4s@users.noreply.github.com>
2024-11-10 13:58:57 +0200

    Created a starting page and a navigation bar

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/contact', function () {
+    return view('contact');  // Return the contact view
+});
+
 require __DIR__.'/auth.php';
